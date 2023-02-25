4e5939079aa8412c8a6a84973908af4d729d8391
Shifali U <54653498+ushifali@users.noreply.github.com>
2023-02-25 14:49:35 +0530

    uploaded consumer and producer

--- consumer.py
+++ consumer.py
@@ -1,16 +1,47 @@
 from kafka import KafkaConsumer
+import json
+import random
 
-# Create Kafka consumer with topic 'test' and connect to Kafka broker at localhost:9092
-consumer = KafkaConsumer('test', bootstrap_servers=['localhost:9092'], auto_offset_reset='latest')
+# Set up Kafka consumer for guesses and messages
+guess_consumer = KafkaConsumer("guesses", bootstrap_servers=["localhost:9092"])
 
-# # Continuously listen for messages on the 'test' topic
-for message in consumer:
-    # Decode the message value (which is in bytes) to a string and convert to integer
-    value = message.value.decode('utf-8')
+# Generate a random number for the player to guess
+secret_number = random.randint(1, 100)
 
-    # Print the received guess
-    if value.isdigit():
-        guess = int(value)
-        print("Received guess:", guess)
-    else:
-        print("Invalid guess received:", value)
+# Initialize the number of guesses made
+num_guesses = 0
+
+
+# Listen for guesses from the producer and respond with hints accordingly
+for guess in guess_consumer:
+    guess_data = json.loads(guess.value.decode())
+    guess_value = guess_data["value"]
+
+    # Increment the number of guesses made
+    num_guesses += 1
+
+    if guess_value == secret_number:
+        # If the player guesses correctly, print a congratulatory message and end the game
+        success_message = f"Congratulations, you guessed the secret number {secret_number} in {num_guesses} guesses!"
+        print(success_message)
+        break
+    elif guess_value < 1 or guess_value > 100:
+        # If the player's guess is out of range, send an error message
+        error_message = {
+            "type": "error",
+            "text": "Invalid guess. Please enter a number between 1 and 100.",
+        }
+        print(error_message["text"])
+    elif num_guesses == 10:
+        # If the player has used up all their guesses, print a failure message and end the game
+        failure_message = f"Sorry, you did not guess the secret number {secret_number} in 10 tries. Better luck next time!"
+        print(failure_message)
+        break
+    elif guess_value < secret_number:
+        # If the player's guess is too low, print a hint message
+        print("Your guess is too low. Guess again.")
+    elif guess_value > secret_number:
+        # If the player's guess is too high, print a hint message
+        print("Your guess is too high. Guess again.")
+        
+    
--- producer.py
+++ producer.py
@@ -1,39 +1,26 @@
 from kafka import KafkaProducer
-import random
+import json
 
 # Set up Kafka producer
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
 
-# Generate a random number between 1 and 100
-secret_number = random.randint(1, 100)
 
-# Initialize the number of guesses
-num_guesses = 0
-
-# Start the game loop
+# Play the game
 while True:
-    # Get a guess from the user
-    guess = input("Enter a number between 1 and 100: ")
+    # Get user input for the next guess
+    guess = input("Enter your guess (or 'exit' to quit): ")
+
+    if guess == "exit":
+        # If the player chooses to exit the game, send a goodbye message and end the game
+        break
+
+    # Convert the guess to an integer
     try:
         guess = int(guess)
-        if guess < 1 or guess > 100:
-            print("Number must be between 1 and 100")
-            continue
     except ValueError:
-        print("Invalid input, please enter a number")
+        # If the guess is not a valid integer, continue
         continue
 
-    # Send the guess to Kafka
-    producer.send('test', str(guess).encode('utf-8'))
-
-    # Check if the guess is correct
-    if guess == secret_number:
-        print("Congratulations, you guessed the secret number! You needed", num_guesses, "guesses.")
-        break
-        
-    # If the guess is incorrect, give a hint and continue the loop
-    if guess < secret_number:
-        print("The secret number is greater.")
-    else:
-        print("The secret number is smaller.")
-    num_guesses += 1
+    # Send the player's guess to the consumer
+    guess_message = {"type": "guess", "value": guess}
+    producer.send("guesses", value=json.dumps(guess_message).encode())
